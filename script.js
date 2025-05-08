07c8f92279ea20f111cd95b57ea33dad010bb023
Farhan Reynaldo <farhanreynaldo@gmail.com>
2025-05-08 14:15:45 +0700

    Add getHumanChoice and playRound functions

--- script.js
+++ script.js
@@ -1,12 +1,54 @@
-function getComputerChoices() {
+function getComputerChoice() {
     num = Math.random()
     if (num < 0.33) {
-        return "Rock";
+        return "rock";
     } else if (num < 0.67) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
-console.log(getComputerChoices())
+function getHumanChoice() {
+    choice = prompt("Choose between rock, paper, or scissors"); 
+    return choice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+    if (humanChoice == "rock") {
+        if (computerChoice == 'paper') {
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+            computerScore++;
+        } else if (computerChoice == "scissors") {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+            humanScore++;
+        } else {
+            console.log(`It's a draw!`)
+        }
+    } else if (humanChoice == 'scissors') {
+        if (computerChoice == 'paper') {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+            humanScore++;
+        } else if (computerChoice == "rock") {
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+            computerScore++;
+        } else {
+            console.log(`It's a draw!`)
+        }
+    } else {
+        if (computerChoice == 'scissors') {
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+            computerScore++;
+        } else if (computerChoice == "rock") {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+            humanScore++;
+        } else {
+            console.log(`It's a draw!`)
+        }
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
