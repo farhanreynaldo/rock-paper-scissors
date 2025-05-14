1f3b6926f847d6ddf261b04ff472a1cbe9e45338
Farhan Reynaldo <farhanreynaldo@gmail.com>
2025-05-14 22:31:55 +0700

    Simplify play round check

--- script.js
+++ script.js
@@ -1,57 +1,53 @@
 function getComputerChoice() {
-    num = Math.random()
-    if (num < 0.33) {
-        return "rock";
-    } else if (num < 0.67) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
+  num = Math.random();
+  if (num < 0.33) {
+    return "rock";
+  } else if (num < 0.67) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
 }
 
 function getHumanChoice() {
-    choice = prompt("Choose between rock, paper, or scissors"); 
-    return choice;
+  choice = prompt("Choose between rock, paper, or scissors");
+  return choice;
+}
+
+function checkWinner() {
+  if (humanScore >= 5 || computerScore >= 5) {
+    if (humanScore >= 5) {
+      score.textContent = "You win!";
+    } else {
+      score.textContent = "You lose!";
+    }
+    humanScore = 0;
+    computerScore = 0;
+  }
 }
 
 function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    computerChoice = computerChoice.toLowerCase();
+  humanChoice = humanChoice.toLowerCase();
+  computerChoice = computerChoice.toLowerCase();
 
-    if (humanChoice == "rock") {
-        if (computerChoice == 'paper') {
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-            computerScore++;
-        } else if (computerChoice == "scissors") {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-            humanScore++;
-        } else {
-            console.log(`It's a draw!`)
-        }
-    } else if (humanChoice == 'scissors') {
-        if (computerChoice == 'paper') {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-            humanScore++;
-        } else if (computerChoice == "rock") {
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-            computerScore++;
-        } else {
-            console.log(`It's a draw!`)
-        }
-        
-    } else { // paper
-        if (computerChoice == 'scissors') {
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-            computerScore++;
-        } else if (computerChoice == "rock") {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-            humanScore++;
-        } else {
-            console.log(`It's a draw!`)
-        }
-    }
+  if (
+    (humanChoice == "rock") & (computerChoice == "paper") ||
+    (humanChoice == "paper") & (computerChoice == "scissors") ||
+    (humanChoice == "scissors") & (computerChoice == "rock")
+  ) {
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    computerScore++;
+  } else if (
+    (humanChoice == "paper") & (computerChoice == "rock") ||
+    (humanChoice == "scissors") & (computerChoice == "paper") ||
+    (humanChoice == "rock") & (computerChoice == "scissors")
+  ) {
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    humanScore++;
+  } else {
+    console.log(`It's a draw!`);
+  }
 }
-
 
 let humanScore = 0;
 let computerScore = 0;
@@ -59,23 +55,13 @@ let computerScore = 0;
 const result = document.querySelector("#result");
 const score = document.createElement("p");
 
-function checkWinner(humanScore, computerScore) {
-    if (humanScore >= 5 || computerScore >= 5) {
-        if (humanScore >= 5) {
-            score.textContent = "You win!";
-        } else {
-            score.textContent = "You lose!";
-        }
-    }
-};
-
 const buttons = document.querySelectorAll("button");
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
     computerChoice = getComputerChoice();
-        playRound(button.id, computerChoice);
+    playRound(button.id, computerChoice);
     score.textContent = `Human score = ${humanScore}; computerScore = ${computerScore}`;
     result.appendChild(score);
     checkWinner(humanScore, computerScore);
-    })
+  });
 });
